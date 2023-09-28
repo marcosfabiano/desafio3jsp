da2304ef0a797b3955942bd0613039225aa091cb
Marcos Fabiano <marcos.fabiano67@outlook.com>
2023-09-28 15:04:28 -0300

    Revert "GET clients paged"
    This reverts commit a3beee87631b4d5f4cad8a4fecbe0c0b7c04ee81.

--- src/main/java/com/marcosfabiano/desafio3jsp/services/ClientService.java
+++ src/main/java/com/marcosfabiano/desafio3jsp/services/ClientService.java
@@ -4,12 +4,8 @@ import com.marcosfabiano.desafio3jsp.dto.ClientDTO;
 import com.marcosfabiano.desafio3jsp.entities.Client;
 import com.marcosfabiano.desafio3jsp.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class ClientService {
@@ -20,11 +16,5 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client client = repository.findById(id).get();
         return new ClientDTO(client);
-    }
-
-    @Transactional(readOnly = true)
-    public Page<ClientDTO> findAll(Pageable pageable) {
-        Page<Client> result = repository.findAll(pageable);
-        return result.map(x -> new ClientDTO(x));
     }
 }
